7fa331c180b273674a4e080dd6525c3e8356aa44
Richard Kneale <richardkneale@hotmail.co.uk>
2019-04-17 21:18:40 +0100

    Updated web sockets example program

---
+++ Service/Service/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.WebSockets;
+
+namespace Evoke.Kiosk.Nordic
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        private ServiceProvider _serviceProvider;
+
+        public Startup()
+        {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddJsonFile(@"appsettings.json");
+
+            _configuration = configurationBuilder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_configuration);
+
+            services.AddCors();
+
+            _serviceProvider = services.BuildServiceProvider();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app,
+            IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(bldr => bldr.WithOrigins("http://localhost:8080")
+                .WithMethods("GET", "POST")
+                .AllowAnyHeader());
+
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
+            };
+
+            app.UseWebSockets(webSocketOptions);
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/message")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                        // TODO: Implement MessageService
+                        //IMessageService nordicService = new MessageService(webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.UseFileServer();
+            app.UseMvc();
+        }
+    }
+}
